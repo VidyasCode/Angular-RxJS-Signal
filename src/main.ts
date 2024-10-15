5ebf94d160a5c29d812f592c2e77511a7b7041c8
VidyasCode <vidyaharnal05@gmail.com>
2024-10-14 22:11:51 -0400

    adding basic rxjs from, of

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { keyframes } from '@angular/animations';
+import { Component, DestroyRef, OnDestroy, OnInit } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import {  Subscription, from, fromEvent, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +13,56 @@ import { bootstrapApplication } from '@angular/platform-browser';
     </a>
   `,
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   name = 'Angular';
-}
 
+  sub! : Subscription;
+  subArray!: Subscription;
+  subFrom! : Subscription;
+  subStrings! : Subscription;
+  subEvent! : Subscription;
+  subKey! : Subscription;
+
+  ngOnInit() {
+
+    // this.sub = of(2,4,6,8).subscribe(item => { console.log(`Value from of : ${item}`)}) ;
+    // this.subArray = of([2,4,6,8]).subscribe(item =>console.log(`Value from of array : ${item}`))
+    // this.subFrom = from([20,15,10,5]).subscribe({
+    //   next: item => console.log('Value from From:', item),
+    //   error: err => console.log('From error:', err),
+    //   complete: () => console.log('No more apples, go home')
+    // });
+    // this.subStrings = of('APPLE1', 'APPLE2', 'APPLE3').subscribe({
+    //   next : item => console.log('hhh', item),
+    //   error: err => console.log('ssss', err),
+    //   complete: ()=> console.log('No more apples, go home')
+
+    // });
+
+    this.subEvent = fromEvent(document, 'click').subscribe({
+      next: ev => console.log('CLick event', ev.target),
+
+    });
+
+    const keys: string[]=[];
+this.subKey = fromEvent(document, 'keydown').subscribe(
+  ev => { 
+    keys.push( (ev as KeyboardEvent).key);
+    console.log('Key Array:', keys);
+  
+  });
+
+  
+  }
+  ngOnDestroy()
+  {
+    this.sub.unsubscribe();
+    this.subArray.unsubscribe();
+    this.subFrom.unsubscribe();
+    this.subStrings.unsubscribe();
+    this.subKey.unsubscribe();
+
+  }
+}
 bootstrapApplication(App);
+ 
